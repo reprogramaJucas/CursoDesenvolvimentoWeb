98444a601b145596c10409a3a41aa41fcdf9fc46
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-08 18:18:54 -0300

    add cbase class 02 operators

---
+++ JavaSript/class-02 -operators/scripts.js
@@ -0,0 +1,15 @@
+let economizarDinheiro = true
+let juntarDinheiro = false
+ 
+// console.log(economizarDinheiro && juntarDinheiro) 
+ 
+
+ let guardarSelic = true
+ let pegarEmprestimo = false
+ 
+//  console.log(guardarSelic || pegarEmprestimo) 
+
+  console.log(!guardarSelic)
+
+
+
